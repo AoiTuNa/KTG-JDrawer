8360811a2096f89b816ca830721e9edf562422b3
AoiTuNA <bestspark123@naver.com>
2023-05-30 01:06:18 +0900

    4-2 동영상

--- DrawerView.java
+++ DrawerView.java
@@ -21,41 +21,15 @@ public class DrawerView extends JPanel implements MouseListener, MouseMotionList
         super.paintComponent(g);
 
         for(int i =0; i< nBox; i++){
-            int minX = Math.min(boxes[i].x1,boxes[i].x2);
-            int minY = Math.min(boxes[i].y1,boxes[i].y2);
-            int width = Math.abs(boxes[i].x2-boxes[i].x1);
-            int height = Math.abs(boxes[i].y2-boxes[i].y1);
-
-            g.drawRect(minX, minY, width, height);
-      
+            boxes[i].draw(g);
         }
     }
     @Override
     public void mouseDragged(MouseEvent e) {
+         Graphics g = getGraphics();
+        g.setXORMode(getBackground());// 겹치는 색을 덧칠하지 않음
         
-
-        int minX = Math.min(pBox.x1,pBox.x2);
-        int minY = Math.min(pBox.y1,pBox.y2);
-        int width = Math.abs(pBox.x2-pBox.x1);
-        int height = Math.abs(pBox.y2-pBox.y1);
-        Graphics g = getGraphics();
-        g.setXORMode(getBackground()); // 겹치는 색을 덧칠하지 않음
-        g.drawRect(minX, minY, width, height);
-        
-///
-
-        pBox.x2 = e.getX();
-        pBox.y2 = e.getY();
-
-        minX = Math.min(pBox.x1,pBox.x2);
-        minY = Math.min(pBox.y1,pBox.y2);
-        width = Math.abs(pBox.x2-pBox.x1);
-        height = Math.abs(pBox.y2-pBox.y1);
-
-        g.setColor(Color.BLACK);
-        g.drawRect(minX, minY, width, height);
-
-        
+        pBox.drawing(g,e.getX(), e.getY());
     }
     @Override
     public void mouseMoved(MouseEvent e) {
@@ -72,16 +46,9 @@ public class DrawerView extends JPanel implements MouseListener, MouseMotionList
     }
     @Override
     public void mouseReleased(MouseEvent e) {
-        pBox.x2 = e.getX();
-        pBox.y2 = e.getY();
-
-        int minX = Math.min(pBox.x1, pBox.x2);
-        int minY = Math.min(pBox.y1, pBox.y2);
-        int width = Math.abs(pBox.x2-pBox.x1);
-        int height = Math.abs(pBox.y2-pBox.y1);
-
-        Graphics g = getGraphics();
-        g.drawRect(minX, minY, width, height);
+        pBox.setXY2(e.getX(),e.getY());
+        Graphics g = getGraphics(); 
+        pBox.draw(g);
     }
     @Override
     public void mouseEntered(MouseEvent e) {
